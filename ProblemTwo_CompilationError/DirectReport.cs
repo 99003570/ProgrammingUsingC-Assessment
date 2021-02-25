fb237b7f25aa80e29c07bc5b7371765277179c43
99003570 <78849735+99003570@users.noreply.github.com>
2021-02-25 16:38:53 +0530

    Add files via upload

--- ProblemTwo_CompilationError/DirectReport.cs
+++ ProblemTwo_CompilationError/DirectReport.cs
@@ -6,15 +6,33 @@ using System.Threading.Tasks;
 
 namespace ProblemTwo_CompilationError
 {
-    class DirectReport
+    class DirectReport : Employee
+        
     {
+        private static int eID;
+        private static string eName;
+        private static int eLevel;
+        private static DateTime eDoJ;
+        
+        public int id { get; set;}
+   
         public Employee ReportsTo { get; set; }
         public string ProjectCode { get; set; }
 
-        public DirectReport(Employee managerEmpObj,string pCode) :base(eID,eName,eLevel,eDoJ)
+        public DateTime dt { get; set; }
+
+        public string branch { get; set; }
+
+        public string status { get; set; }
+
+        public DirectReport(int id, Employee managerEmpObj, string pCode, DateTime dt, string status, string branch) : base(eID, eName, eLevel, eDoJ)
         {
             ReportsTo = managerEmpObj;
             ProjectCode = pCode;
+        }
+
+        public DirectReport(int eID, string eName, int eLevel, DateTime eDoJ, object p, string v) : base(eID, eName, eLevel, eDoJ)
+        {
         }
     }
 }
